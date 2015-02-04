7c1efedc1de1f545fe7262805ec7c3436c8c0e45
Chad Hendry <chendry@chendry.org>
2015-02-04 13:26:17 -0600

    adding airootfs-private

--- prepare_and_build.sh
+++ prepare_and_build.sh
@@ -1,5 +1,20 @@
 #!/bin/bash -x
+
 cd `dirname $0`
+
 cp -r /var/cache/pacman/pkg ./airootfs/root/packages
+
+mkdir -p ./airootfs/root/rootfs-private
+
+for i in \
+  /home/chendry/.ssh \
+  /home/chendry/.gnupg \
+  /etc/postfix/sasl_passwd.db \
+  /usr/lib/firmware/b43 \
+  /etc/netctl/wlp4s0b1-Chad
+do
+  cp --parents -r $i ./airootfs/root/rootfs-private
+done
+
 sudo rm -rf ./work
 sudo ./build.sh -v
