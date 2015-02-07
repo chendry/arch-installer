559eeabb630a1d0bcf130961950d079f6784b6a8
Chad Hendry <chendry@chendry.org>
2015-02-06 22:05:36 -0600

    installing dispad

--- install-scripts/120-xf86-input-mtrack.sh
+++ install-scripts/120-xf86-input-mtrack.sh
@@ -9,3 +9,17 @@ cat <<-END | arch-chroot /mnt
 pacman --noconfirm -U ~chendry/xf86-input-mtrack-git/*.tar.xz
 rm -rf ~chendry/xf86-input-mtrack-git*
 END
+
+cat <<-END | arch-chroot /mnt su -l chendry
+wget https://aur.archlinux.org/packages/di/dispad-git/dispad-git.tar.gz
+tar -zxvf dispad-git.tar.gz
+cd dispad-git
+makepkg -s --noconfirm
+END
+
+cat <<-END | arch-chroot /mnt
+pacman --noconfirm -U ~chendry/dispad-git/*.tar.xz
+rm -rf ~chendry/dispad-git*
+END
+
+
